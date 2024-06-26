5a855f2287d28d12210de078cbd3b09986742b54
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-06-26 23:17:33 +0000

    task#1: fifo cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""1-fifo_cache.py"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """a fifo cache system"""
+    def __init__(self):
+        """instantiate an object"""
+        super().__init__()
+        self.fifo = []  # stores the keys to mimic the FIFO algorithm
+
+    def put(self, key, item):
+        """Add an item to the cache using FIFO algo"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            k = self.fifo.pop(0)
+            self.cache_data.pop(k)
+            print(f"DISCARD: {k}")
+        self.cache_data.update({key: item})
+        self.fifo.append(key)
+
+    def get(self, key):
+        """Get an item from the cache"""
+        if key is None:
+            return None
+        self.cache_data.get(key)
+
