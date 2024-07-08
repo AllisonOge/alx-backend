51a3d2f0773d3b28a714ff0ab5c0213a4e0d70d0
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2024-07-08 01:17:44 +0000

    task#6: use user locale

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Mocking login
+"""
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g,
+    Response
+)
+from flask_babel import Babel
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    The order of priority should be
+
+    Locale from URL parameters
+    Locale from user settings
+    Locale from request header
+    Default locale
+    """
+    if request.args.get("locale") is not None:
+        return request.args["locale"]
+    if g.user \
+        and g.user.get("locale") is not None \
+            and g.user.get("locale") in app.config.get("LANGUAGES"):
+        return g.user["locale"]
+    return request.accept_languages.best_match(app.config.get("LANGUAGES"))
+
+
+def get_user() -> dict | None:
+    """Returns a user dictionary or None"""
+    user_id = request.args.get("login_as", "-1")
+    return users.get(int(user_id)) if user_id.isdigit() else None
+
+
+@app.before_request
+def before_request():
+    g.user = get_user()
+
+
+@app.route("/")
+def root() -> Response:
+    return render_template("6-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
